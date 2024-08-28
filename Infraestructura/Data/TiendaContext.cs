9d948182119d887fd0d7d956994c3e6a72a9ac62
José L. Gutiérrez <gutiecuador@gmail.com>
2024-08-28 10:56:44 -0500

    Agregar archivos de proyecto.

---
+++ Infraestructura/Data/TiendaContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
